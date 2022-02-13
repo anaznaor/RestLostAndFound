585457d6f7a85cf970131cb79db6f55d8c874994
AnaZ <znaor.a@gmail.com>
2022-02-13 22:46:57 +0100

    fixing bugs

--- src/main/java/hr/fer/ruazosa/lostnfound/repository/NotificationRepository.java
+++ src/main/java/hr/fer/ruazosa/lostnfound/repository/NotificationRepository.java
@@ -2,10 +2,7 @@ package hr.fer.ruazosa.lostnfound.repository;
 
 import hr.fer.ruazosa.lostnfound.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
